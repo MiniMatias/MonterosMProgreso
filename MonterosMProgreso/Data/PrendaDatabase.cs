7852a32b022b8380a533900e761860284eb89b6f
Matias Monteros <matias.monteros@udla.edu.ec>
2025-07-08 07:56:48 -0500

    creacion de archivos clave con su codigo

---
+++ MonterosMProgreso/Data/PrendaDatabase.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using MonterosMProgreso.Models;
+
+namespace MonterosMProgreso.Data
+{
+    public class PrendaDatabase
+    {
+        private readonly SQLiteAsyncConnection _database;
+        public PrendaDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Prenda>().Wait();
+        }
+        public Task<List<Prenda>> GetPrendasAsync() => _database.Table<Prenda>().ToListAsync();
+        public Task<int> SavePrendaAsync(Prenda prenda) => _database.InsertAsync(prenda);
+    }
+}
---
+++ MonterosMProgreso/Models/Prenda.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace MonterosMProgreso.Models
+{
+    public class Prenda
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Color { get; set; }
+        public int Talla { get; set; }
+        public bool EnInventario { get; set; }
+
+    }
+}
