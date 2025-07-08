cdbe6dd72a51cc6c537dc01e8aa599fb8f5f4965
Matias Monteros <matias.monteros@udla.edu.ec>
2025-07-08 07:35:30 -0500

    Creacion de carpetas y logica de codigo

---
+++ MonterosMProgreso/ViewModels/ListaPrendasViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using MonterosMProgreso.Models;
+using MonterosMProgreso.Data;
+
+namespace MonterosMProgreso.ViewModels;
+
+public class ListaPrendasViewModel : INotifyPropertyChanged
+{
+    public ObservableCollection<Prenda> Prendas { get; } = new();
+    private readonly PrendaDatabase _database;
+
+    public ICommand CargarCommand { get; }
+
+    public ListaPrendasViewModel()
+    {
+        _database = App.Current.Services.GetService<PrendaDatabase>();
+        CargarCommand = new Command(async () => await Cargar());
+        _ = Cargar(); // carga automática al iniciar
+    }
+
+    public async Task Cargar()
+    {
+        var lista = await _database.GetPrendasAsync();
+        Prendas.Clear();
+        foreach (var prenda in lista)
+            Prendas.Add(prenda);
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
---
+++ MonterosMProgreso/ViewModels/LogsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace InventarioRopa_Monteros.ViewModels;
+
+public class LogsViewModel : INotifyPropertyChanged
+{
+    public ObservableCollection
+<string> Registros
+    { get; } = new();
+
+    public LogsViewModel()
+    {
+        CargarLogs();
+    }
+
+    private void CargarLogs()
+    {
+        string ruta = Path.Combine(FileSystem.AppDataDirectory, "Logs_Monteros.txt");
+        if (File.Exists(ruta))
+        {
+            var lineas = File.ReadAllLines(ruta);
+            foreach (var linea in lineas)
+                Registros.Add(linea);
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
